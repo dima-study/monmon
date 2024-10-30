accc67d72d6af8e35ded0005ae78b76b2d14c9cc
Dmitry Latin <dim0xff@gmail.com>
2024-10-30 14:11:34 +0300

    add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,50 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+)
+
+const (
+	LevelTrace = slog.Level(-8)
+)
+
+// Logger - логгер вокруг slog.Logger, задача которого - добавить уровень LevelTrace
+type Logger struct {
+	*slog.Logger
+}
+
+func New(h slog.Handler) *Logger {
+	l := Logger{
+		Logger: slog.New(h),
+	}
+
+	return &l
+}
+
+func (l *Logger) Trace(msg string, args ...any) {
+	l.TraceContext(context.Background(), msg, args...)
+}
+
+func (l *Logger) TraceContext(ctx context.Context, msg string, args ...any) {
+	l.Log(ctx, LevelTrace, msg, args...)
+}
+
+func (l *Logger) With(args ...any) *Logger {
+	return &Logger{
+		Logger: l.Logger.With(args...),
+	}
+}
+
+// ReplaceAttrLevel использовать в HandlerOptions.ReplaceAttr,
+// чтобы корректно отображать уровень LevelTrace
+func ReplaceAttrLevel(groups []string, a slog.Attr) slog.Attr {
+	if a.Key == slog.LevelKey {
+		level := a.Value.Any().(slog.Level)
+		if level == LevelTrace {
+			a.Value = slog.StringValue("TRACE")
+		}
+	}
+
+	return a
+}
